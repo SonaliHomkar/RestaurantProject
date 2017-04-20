b04d4c606ee5bc0b2ba101ed959643f2419f1adb
SonaliHomkar <sonali.hirve@gmail.com>
2017-04-20 14:05:29 -0700

    Add files via upload
    added menuitem add/edit and delete

--- webserver.py
+++ webserver.py
@@ -63,7 +63,9 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "<html><body>"
                 output += "<a href='/restaurant/new'><b>Make a new Restaurant</b></a><br>"
                 for i in restaurants:
-                    output += "<b>" + i.name + "</b>"
+                    output += "<b>" + i.name + "</b>" 
+                    output += "<br>"
+                    output += "<b><a href='/restaurant/%s/Menu'>Menu </a>" % str(i.id) 
                     output += "<br>"
                     output += "<a href='/restaurant/%s/edit' >Edit </a>" % str(i.id)
                     output += "<br>"
@@ -87,6 +89,99 @@ class webserverHandler(BaseHTTPRequestHandler):
 
                 self.wfile.write(output)
                 print output
+            # 19 Apr code #
+            if self.path.endswith("/Menu"):
+                resturantId = self.path.split("/")[2]
+                Menuitems = session.query(MenuItem).filter_by(restaurant_id = resturantId).all()
+                self.send_response(200)
+                self.send_header('content-type','text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "<a href='/restaurant/%s/newMenu'><b> Add a new Menu</b></a><br>" % resturantId
+                output += "<a href='/restaurant'><b> Cancel</b></a><br>"
+                if Menuitems != []:
+                    for i in Menuitems:
+                        output += "<b>" + i.name + "</b>" 
+                        output += "<br>"
+                        output += "<a href='/restaurant/%s/editMenu' >Edit </a>" % str(i.id)
+                        output += "<br>"
+                        output += "<a href='/restaurant/%s/deleteMenu' >Delete </a>" % str(i.id)
+                        output += "<br><br>"
+                else:
+                    output += "<b> No MenuItems found..please add it</b>" 
+                output += "</body></html>"
+
+                self.wfile.write(output)
+                print output
+
+            if self.path.endswith("/newMenu"):
+                self.send_response(200)
+                self.send_header('content-type','text/html')
+                self.end_headers()
+
+                resturantid = self.path.split("/")[2]
+                output = ""
+                output += "<html><body>"
+                output += "<form method = 'POST' enctype= 'multipart/form-data' action='/restaurant/%s/newMenu'>" % str(resturantid)
+                output += "<table><td colspan =3><b>Menu Item</b></td>"
+                output += "<tr><td>Name</td><td>:</td><td><input type='text' name='txtMenuItemName'></td></tr>"
+                output += "<tr><td>Description</td><td>:</td><td><input type='text' name='txtDescription'></td></tr>"
+                output += "<tr><td>Price</td><td>:</td> <td><input type='text' name='txtPrice'></td></tr>"
+                output += "<tr><td>Course</td><td>:</td><td><input type='text' name='txtCourse'></td></tr>"
+                output += "<tr><td><a href='/restaurant/%s/Menu'>Cancel</a></td><td><input type='submit' value='submit'></td></tr>" % str(resturantid) 
+                output += "</table>"
+                output += "</form></html></body>"
+
+                self.wfile.write(output)
+            #end 19 Apr code #
+            # start 20 apr code #
+            if self.path.endswith("/editMenu"):
+                self.send_response(200)
+                self.send_header('content-type','text/html')
+                self.end_headers()
+
+                menuId = self.path.split("/")[2]
+                MenuItems = session.query(MenuItem).filter_by(id = menuId).all()
+                output = ""
+                output += "<html><body>"
+                output += "<form method = 'POST' enctype= 'multipart/form-data' action='/restaurant/%s/editMenu'>" % str(menuId)
+                if MenuItems != []:
+                    for i in MenuItems:
+                        output += "<table><td colspan =3><b>Menu Item</b></td>"
+                        output += "<tr><td>Name</td><td>:</td><td><input type='text' name='txtMenuItemName' placeholder='%s'></td></tr>" % i.name
+                        output += "<tr><td>Description</td><td>:</td><td><input type='text' name='txtDescription' placeholder='%s'></td></tr>" % i.description
+                        output += "<tr><td>Price</td><td>:</td> <td><input type='text' name='txtPrice' placeholder='%s'></td></tr>" % i.price
+                        output += "<tr><td>Course</td><td>:</td><td><input type='text' name='txtCourse' placeholder='%s'></td></tr>" % i.course
+                        output += "<tr><td><a href='/restaurant/%s/Menu'>Cancel</a></td><td><input type='submit' value='submit'></td></tr>" % str(i.restaurant_id)
+                else:
+                   output += "<b>No details found</b>" 
+                output += "</table>"
+                output += "</form></html></body>"
+
+                self.wfile.write(output)
+                
+            # end 20 apr code #
+            # start 20 apr code #
+            if self.path.endswith("/deleteMenu"):
+                menuId = self.path.split("/")[2]
+                menuItemquery = session.query(MenuItem).filter_by(id = menuId).one()
+                if menuItemquery != []:
+                    self.send_response(200)
+                    self.send_header('content-type' , 'text/html')
+                    self.end_headers()
+                    output = ""
+                    output += "<html><body>"
+                    output += "<h1>Are you sure you want to Menu item %s</h1>" % menuItemquery.name
+                    output += "<form method = 'POST' enctype= 'multipart/form-data' action='/restaurant/%s/deleteMenu'>" % menuId
+                    output +=  "<input type='submit' value='Delete'>"
+                    output += "<a href='/restaurant/%s/Menu'>Cancel</a>" % menuItemquery.restaurant_id
+                    output += "</form></html></body>"
+
+                    self.wfile.write(output)
+                
+            # end 20 apr code #
                 
             if self.path.endswith("/hello"):
                 self.send_response(200)
@@ -180,7 +275,66 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "<h2>What would you like me to say?</h2><input name='message'"
                 output += " type='text'><input type='submit' value='submit'></form>"
                 output += "</body></html>"
-                self.wfile.write(output)   
+                self.wfile.write(output)
+            # 19 Apr code #
+            if self.path.endswith("/newMenu"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    fldname = fields.get('txtMenuItemName')
+                    flddescription = fields.get('txtDescription')
+                    fldprice = fields.get('txtPrice')
+                    fldcourse = fields.get('txtCourse')
+                    restaurantId = self.path.split("/")[2]
+
+                    NewMenuItemQuery = MenuItem(name = fldname[0], description = flddescription[0],
+                                                price = fldprice[0], course = fldcourse[0], restaurant_id = restaurantId )
+                    session.add(NewMenuItemQuery)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header('content-type','text/html')
+                    self.send_header('Location','restaurant/%s/Menu' % str(restaurantId))
+                    self.end_headers()
+            # end 19 Apr code #
+            # start 20 apr code #
+            if self.path.endswith("/editMenu"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    fldname = fields.get('txtMenuItemName')
+                    flddescription = fields.get('txtDescription')
+                    fldprice = fields.get('txtPrice')
+                    fldcourse = fields.get('txtCourse')
+                    menuItemId = self.path.split("/")[2]
+                    editMenuItemQuery = session.query(MenuItem).filter_by(id = menuItemId).one()
+                    restaurantId = editMenuItemQuery.restaurant_id
+                    if editMenuItemQuery != []:
+                        editMenuItemQuery.name = fldname[0]
+                        editMenuItemQuery.description = flddescription[0]
+                        editMenuItemQuery.price = fldprice[0]
+                        editMenuItemQuery.course = fldcourse[0]
+                        session.add(editMenuItemQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('content-type','text/html')
+                        self.send_header('Location','/restaurant/%s/Menu' % str(restaurantId))
+                        self.end_headers()
+            # end 20 apr code #
+            # start 20 apr code #
+            if self.path.endswith("/deleteMenu"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                menuId = self.path.split("/")[2]
+                menuDeleteQuery = session.query(MenuItem).filter_by(id = menuId).one()
+
+                if menuDeleteQuery != []:
+                    session.delete(menuDeleteQuery)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header('content-type','text/html')
+                    self.send_header('Location','/restaurant')
+                    self.end_headers()
+                
+            # end 20 apr code #
         except:
             pass
     
